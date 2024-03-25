a33649ca764285f497bb230f9bd231e24e296bd2
Hans Naert <hans.naert@vives.be>
2024-03-25 15:26:36 +0100

    Add listview and onclick handler

--- app/src/main/java/com/example/mybasicviewsapplication/ThirdFragment.kt
+++ app/src/main/java/com/example/mybasicviewsapplication/ThirdFragment.kt
@@ -1,10 +1,12 @@
 package com.example.mybasicviewsapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.navigation.fragment.findNavController
 import com.example.mybasicviewsapplication.databinding.FragmentSecondBinding
 import com.example.mybasicviewsapplication.databinding.FragmentThirdBinding
@@ -13,6 +15,8 @@ import com.example.mybasicviewsapplication.databinding.FragmentThirdBinding
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class ThirdFragment : Fragment() {
+
+    val stringList = listOf("First", "Second", "Third", "Fourth")
 
     private var _binding: FragmentThirdBinding? = null
 
@@ -34,6 +38,14 @@ class ThirdFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonThird.setOnClickListener {
+            findNavController().navigate(R.id.action_thirdFragment_to_fourthFragment)
+        }
+
+        val arrayAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, stringList)
+        binding.listView.adapter = arrayAdapter
+        binding.listView.setOnItemClickListener { parent, view, position, id ->
+            val item = stringList[position]
+            Log.v("LISTVIEW","Item clicked: $item")
             findNavController().navigate(R.id.action_thirdFragment_to_fourthFragment)
         }
     }
